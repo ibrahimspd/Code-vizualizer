e9df66eb770d0de407ff701b760aeae272c34d14
Pontusg45 <62245536+Pontusg45@users.noreply.github.com>
2023-03-27 11:57:41 +0200

    update parser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,33 @@
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import endpoints.GraphController;
 import entites.*;
 import io.javalin.Javalin;
 import io.javalin.http.UploadedFile;
 import io.javalin.util.FileUtil;
-import org.eclipse.jetty.plus.jndi.Link;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 
 public class Main
 {
-
     public static void main(String[] args)
     {
         Javalin app = Javalin.create(/**/).start(8080);
+        app.before(ctx -> ctx.header("Access-Control-Allow-Origin", "*"));
         app.post("/parse", context ->
         {
+            long start = System.nanoTime();
+
             UploadedFile uploadedFile = context.uploadedFiles().get(0);
             try (InputStream inputStream = uploadedFile.content())
             {
@@ -37,16 +36,14 @@ public class Main
             {
                 e.printStackTrace();
             }
-            Map<Object, Files> filesMap = new HashMap<>();
-            Map<Object, Classes> classesMap = new HashMap<>();
-            Map<String, FunctionCall> functionCalls = new HashMap<>();
+            Map<Object, Classes> classesMap = new ConcurrentHashMap<>();
+            Map<String, FunctionCall> functionCalls = new ConcurrentHashMap<>();
             JsonObject newFilesMap = new JsonObject();
-            try (ZipFile zip = new ZipFile("/tmp/fil.zip"))
-            {
+            List<CompilationUnit> fileData = new ArrayList<>();
+            Gson gson = new Gson();
 
-                Enumeration<? extends ZipEntry> filesFromZip = zip.entries();
-                Enumeration<? extends ZipEntry> secondEntry = zip.entries();
-
+            try (ZipFile zipFile = new ZipFile("/tmp/fil.zip")) {
+                Enumeration<? extends ZipEntry> filesFromZip = zipFile.entries();
                 while (filesFromZip.hasMoreElements())
                 {
                     ZipEntry fileFromZip = filesFromZip.nextElement();
@@ -54,142 +51,108 @@ public class Main
                     String filePath = fileFromZip.getName();
                     if (filePath.endsWith(".java"))
                     {
-                        InputStream inputStream = zip.getInputStream(fileFromZip);
-                        Scanner scanner = new Scanner(inputStream);
-                        StringBuilder javaCode = new StringBuilder();
-                        while (scanner.hasNext())
-                        {
-                            String nl = scanner.nextLine();
-                            javaCode.append(nl + "\n");
-
-                        }
-                        CompilationUnit cu = StaticJavaParser.parse(javaCode.toString());
-
-                        cu.findAll(ClassOrInterfaceDeclaration.class).forEach(classOrInterfaceDeclaration ->
-                        {
-                            List<MethodDeclaration> methodDeclarations = classOrInterfaceDeclaration.getMethods();
-                            Map<Object, Object> parametersMap = new HashMap<>();
-                            Map<Object, Function> functions = new HashMap<>();
-                            Map<Object, Classes> classesHashMap = new HashMap<>();
-                            methodDeclarations.forEach(method ->
-                            {
-                                method.getParameters().forEach(parameter ->
+                        InputStream inputStream = zipFile.getInputStream(fileFromZip);
+                        try {
+                            CompilationUnit cu = StaticJavaParser.parse(inputStream);
+                            fileData.add(cu);
+                                cu.findAll(ClassOrInterfaceDeclaration.class).forEach(classOrInterfaceDeclaration ->
                                 {
-                                    parametersMap.put(parameter.getNameAsString(), parameter.getTypeAsString());
-                                });
-                                String functionName = method.getNameAsString();
-                                String functionCallName = classOrInterfaceDeclaration.getNameAsString() + "#" + functionName;
-                                Function function = new Function.Functionbuilder()
-                                        .id(functionCallName)
-                                        .build();
-                                functions.put(method.getNameAsString(), function);
-
-                                FunctionCall functionCall = new FunctionCall
-                                        .FunctionCallBuilder()
-                                        .setChildren(new ArrayList<>())
-                                        .setInComing(0).setOutGoing(0)
-                                        .setNumberOfCalls(0)
-                                        .setParameters(parametersMap)
-                                        .setParents(new ArrayList<>()).setRecursive("false")
-                                        .setName(functionCallName).build();
-                                functionCalls.put(functionCallName, functionCall);
-                            });
-                            Classes classObj = new Classes(functions, classOrInterfaceDeclaration.getNameAsString(), null, UUID.randomUUID().toString(), filePath);
-                            classesMap.put(classObj.getName(), classObj);
-                            classesHashMap.put(classObj.getName(), classObj);
-                            List<String> pathArray = Arrays.asList(filePath.split("/"));
-                            JsonObject walker = newFilesMap;
-                            for (int i = 0;  i < pathArray.size(); i++){
-                                String path = pathArray.get(i);
-                                if(walker == null){
-                                    walker = new JsonObject();
-                                    walker.add(path, new JsonObject());
-                                }
-                                else if (!walker.has(path)) {
-                                    walker.add(path, new JsonObject());walker = (JsonObject) walker.get(path);
-                                }
-                                if(i == pathArray.size() - 1){
-                                    walker.addProperty("name", classOrInterfaceDeclaration.getNameAsString());
-                                    walker.add("class", new Gson().toJsonTree(classObj));
-                                }
-                                walker = (JsonObject) walker.get(path);
-
-                            }
-
-                        });
-                    }
-                }
-                System.out.println(newFilesMap);
-
-                while (secondEntry.hasMoreElements())
-                {
-                    ZipEntry entry = secondEntry.nextElement();
-                    String fileName = entry.getName();
-
-                    if (fileName.endsWith(".java"))
-                    {
-                        InputStream inputStream = zip.getInputStream(entry);
-                        Scanner scanner = new Scanner(inputStream);
-                        StringBuilder javaCode = new StringBuilder();
-                        while (scanner.hasNext())
-                        {
-                            javaCode.append(scanner.nextLine() + "\n");
-                        }
-                        CompilationUnit cu = StaticJavaParser.parse(javaCode.toString());
-                        cu.findAll(MethodCallExpr.class).forEach(methodCallExpr ->
-                        {
-                            classesMap.forEach((s, classes) ->
-                            {
-                                String methodCaller = methodCallExpr.findAncestor(MethodDeclaration.class).isPresent() ? methodCallExpr.findAncestor(MethodDeclaration.class).get().getNameAsString() : null;
-                                String methodClass = methodCallExpr.findAncestor(MethodDeclaration.class).isPresent() ? methodCallExpr.findAncestor(ClassOrInterfaceDeclaration.class).get().getNameAsString() : null;
-                                Function function = classes.getFunctions().get(methodCallExpr.getNameAsString());
-                                if (function != null)
-                                {
-                                    String className = function.getId().split("#")[0];
-                                    String methodName = function.getId().split("#")[1];
-                                    if (methodCallExpr.getNameAsString().equals(methodName))
+                                    List<MethodDeclaration> methodDeclarations = classOrInterfaceDeclaration.getMethods();
+                                    Map<Object, Function> functions = new HashMap<>();
+                                    methodDeclarations.forEach(method ->
                                     {
-                                        FunctionCall functionCall = functionCalls.get(className + "#" + methodCallExpr.getNameAsString());
-                                        String functionCaller = methodClass + "#" + methodCaller;
+                                        Map<Object, Object> parametersMap = new HashMap<>();
+                                        method.getParameters().forEach(parameter ->
+                                                parametersMap.put(parameter.getNameAsString(), parameter.getTypeAsString())
+                                        );
+                                        String functionName = method.getNameAsString();
+                                        String functionCallName = classOrInterfaceDeclaration.getNameAsString() + "#" + functionName;
+                                        Function function = new Function.Functionbuilder()
+                                                .id(functionCallName)
+                                                .build();
+                                        functions.put(functionName, function);
 
-                                        if (!functionCall.getParents().contains(functionCaller))
-                                        {
-                                            functionCall.addParent(functionCaller);
+                                        FunctionCall functionCall = new FunctionCall
+                                                .FunctionCallBuilder()
+                                                .setChildren(new ArrayList<>())
+                                                .setInComing(0).setOutGoing(0)
+                                                .setNumberOfCalls(0)
+                                                .setParameters(parametersMap)
+                                                .setParents(new ArrayList<>()).setRecursive("false")
+                                                .setName(functionCallName).build();
+                                        functionCalls.put(functionCallName, functionCall);
+                                    });
+                                    Classes classObj = new Classes(functions, classOrInterfaceDeclaration.getNameAsString(), null, UUID.randomUUID().toString(), filePath);
+                                    classesMap.put(classObj.getName(), classObj);
+                                    List<String> pathArray = Arrays.asList(filePath.split("/"));
+                                    JsonObject walker = newFilesMap;
+                                    for (int i = 0;  i < pathArray.size(); i++){
+                                        String path = pathArray.get(i);
+                                        if(walker == null){
+                                            walker = new JsonObject();
+                                            walker.add(path, new JsonObject());
                                         }
-                                        functionCall.incrementNumberOfCalls();
-                                        functionCall.incrementInComing();
+                                        else if (!walker.has(path)) {
+                                            JsonObject temp = new JsonObject();
+                                            if(path.endsWith(".java")){
+                                                temp.addProperty("name", classOrInterfaceDeclaration.getNameAsString());
+                                                temp.add("class", gson.toJsonTree(classObj));
+                                            }
+                                            walker.add(path, temp);
+                                        }
+                                        walker = (JsonObject) walker.get(path);
                                     }
-                                }
-                                ;
-                            });
-                        });
+                                });
+                            }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
                     }
                 }
+                for (CompilationUnit cu : fileData){
+                    cu.findAll(MethodCallExpr.class).forEach(methodCallExpr ->
+                        classesMap.forEach((s, classes) ->
+                        {
+                            Optional<MethodDeclaration> classAncestor = methodCallExpr.findAncestor(MethodDeclaration.class);
+                            String methodCaller = classAncestor.isPresent() ? classAncestor.get().getNameAsString() : null;
+                            String methodClass = classAncestor.isPresent() ? methodCallExpr.findAncestor(ClassOrInterfaceDeclaration.class).get().getNameAsString() : null;
+                            String methodeCallExprName = methodCallExpr.getNameAsString();
+                            Function function = classes.getFunctions().get(methodeCallExprName);
+                            if (function != null) {
+                                if (methodeCallExprName.equals(function.getId().split("#")[1]))
+                                {
+                                    FunctionCall functionCall = functionCalls.get(function.getId());
+                                    String functionCaller = methodClass + "#" + methodCaller;
 
-
-            } catch (IOException io)
-            {
+                                    if (!functionCall.getParents().contains(functionCaller))
+                                    {
+                                        functionCall.addParent(functionCaller);
+                                    }
+                                    functionCall.incrementNumberOfCalls();
+                                    functionCall.incrementInComing();
+                                }
+                            }
+                        })
+                    );
+                }
+            } catch (IOException io) {
                 io.printStackTrace();
             }
-            Gson Gson = new Gson();
             functionCalls.forEach((s, function) ->
-            {
                 function.getParents().forEach(parent ->
                 {
-
                     if (functionCalls.containsKey(parent))
                     {
-
                         functionCalls.get(parent).addChild(function.getName());
                     }
-                });
-            });
+                })
+            );
             JsonObject tree = new JsonObject();
 
-
             tree.add("tree", newFilesMap);
-            tree.add("functions", new Gson().toJsonTree(functionCalls));
-            context.result(Gson.toJson(tree));
+            tree.add("functions", gson.toJsonTree(functionCalls));
+
+            context.result(gson.toJson(tree));
         });
     }
 }
